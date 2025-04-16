bc1c3398e7028c0c63df14150c9025a5139c3221
Your Name <Shubhangi Katariyar>
2025-04-17 03:48:29 +0530

    day 1

---
+++ Band-Name-Generator.py
@@ -0,0 +1,6 @@
+print("Hii, Welcome to the Band Name Generator!")
+
+city=input("In which city did you grew up?\n")
+pet=input("What is name of your pet?\n")
+
+print("Congratulations, your Band Name is {} {}".format(city,pet))
